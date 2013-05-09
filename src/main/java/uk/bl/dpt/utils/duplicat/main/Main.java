a9f838e51765c0fa322ec5311ba66018866d7acd
petecliff <pete@pixelatedpete.co.uk>
2013-05-09 09:23:36 +0100

    Removed BDB and streamlined code for Lucene. Lower memory overhead now (250M over 2.7million files rather than 1.5GB). Slightly faster (20 minutes) too.

--- src/main/java/uk/bl/dpt/utils/duplicat/main/Main.java
+++ src/main/java/uk/bl/dpt/utils/duplicat/main/Main.java
@@ -5,76 +5,68 @@ import java.io.IOException;
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.MissingOptionException;
-import org.apache.commons.cli.Option;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
-import uk.bl.dpt.utils.duplicat.entities.Config;
-import uk.bl.dpt.utils.duplicat.execsys.DuplicatException;
-import uk.bl.dpt.utils.duplicat.execsys.DuplicatExecutor;
-import uk.bl.dpt.utils.duplicat.execsys.bdb.BDBExecImpl;
-import uk.bl.dpt.utils.duplicat.execsys.lucene.LuceneExecImpl;
+import uk.bl.dpt.utils.duplicat.exec.Config;
+import uk.bl.dpt.utils.duplicat.exec.LuceneExec;
 
 public class Main {
 	private static long startTime;
 	private static long endTime;
+	private static Options opts;
 
 	public static void main(String[] args) {
 		startTime = System.currentTimeMillis();
-		Options opts = new Options();
-		Option cOpt = new Option("c", true,
+		opts = new Options();
+		opts.addOption("c", true,
 				"The location of the configuration properties file");
-		cOpt.setRequired(true);
-		opts.addOption(cOpt);
 		opts.addOption("l", false,
-				"Lists the content of the dbenv/store given in conf file");
+				"Lists the content of the indexes given in conf file");
 		opts.addOption("f", false, "Create file path list");
 		opts.addOption("i", false, "Create file info list");
-		opts.addOption("p", false, "Purge idatabases before starting");
+		opts.addOption("p", false, "Purge indexes before starting");
 		opts.addOption("m", false, "Purge file list before starting");
 		opts.addOption("n", false, "Purge file info before starting");
-		opts.addOption("r", false, "Run a report, giving name as parameter");
+		opts.addOption("h", false, "Display help message");
+//		opts.addOption("r", false, "Run a report, giving name as parameter");
 
+		System.out.println("Duplicat");
+		System.out.println("--------");
+		
 		CommandLineParser parser = new BasicParser();
 		CommandLine cmd = null;
 		try {
 			cmd = parser.parse(opts, args);
-		} catch (MissingOptionException e) {
-			System.out.println("You must specify a config file using -c FILE");
-			System.exit(-1);
-		} catch (ParseException e) {
-			e.printStackTrace();
-			System.exit(-1);
+		} catch (ParseException pe) {
+			System.out.println("Unable to parse command line: " + pe.getMessage());
+			doUsageAndExit();
 		}
-
+		
+		if ( cmd.hasOption('h') ) {
+			doUsageAndExit();
+		}
+		
+		if (!cmd.hasOption('c') ) {
+			System.out.println(" You MUST specify a config file for all other actions");
+			doUsageAndExit();
+		}
 		
 		Config conf = null;
-
-		System.out.println("Duplicat");
-		System.out.println("--------");
-		System.out.println(" Begin");
-
 		try {
 			conf = new Config(cmd.getOptionValue('c'));
 		} catch (IOException e) {
-			System.out.println("You must specify a config file using -c FILE");
-			System.exit(-1);
+			System.out.println(" You MUST specify a valid config file for all other actions");
+			doUsageAndExit();
 		}
 		
-		DuplicatExecutor mi = null;
-		try {
-			mi = new LuceneExecImpl(conf);
-		} catch (DuplicatException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.exit(-1);
-		}
-
-		if (cmd.hasOption('l')) {
+		LuceneExec mi =  new LuceneExec(conf);
+		
+		if ( cmd.hasOption('l')) {
 			mi.listDatabase();
-		} else if (cmd.hasOption('r')) {
 		} else {
+			// The order of these is important as we step through each sequentially.
 			if (cmd.hasOption('p')) {
 				mi.purgeDatabases();
 			}
@@ -90,16 +82,17 @@ public class Main {
 			if (cmd.hasOption('i')) {
 				mi.createFileInfo();
 			}
-		}
+		} 
 		
 		mi.close();
 
 		endTime = System.currentTimeMillis();
 		float execTime = (endTime - startTime) / 1000;
 
-		System.out.println(" End");
 		System.out.println("--------");
 
+		System.out.println("ExecTime: " + execTime);
+		
 		if (execTime < 60) {
 			System.out.printf("Execution time %8.2f secs\n", execTime);
 		} else if (execTime / 60 < 60) {
@@ -108,5 +101,11 @@ public class Main {
 			System.out.printf("Execution time %8.2f hours\n",
 					execTime / 60 / 60);
 		}
+	}
+	
+	private static void doUsageAndExit() {
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp( "java -jar duplicat.jar", opts );
+		System.exit(0);
 	}
 }
